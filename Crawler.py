b8fbc29aedda1bfde819ff9f40d1d948ce2c8226
unknown <rkfdyddh@gmail.com>
2018-04-06 10:41:08 +0900

    Crawler

--- Crawler.py
+++ Crawler.py
@@ -33,6 +33,8 @@ def cktitle(title):
         return "relation_tag"
     elif title == "발행자":
         return "publisher"
+    elif title == "발행일자":
+        return "publish_date"
     elif title == "기여자":
         return "contributor"
     elif title == "저작물명 대체제목":
@@ -72,6 +74,8 @@ def resultlins(licensepath, path):
 
 def cklicense(license):
     file = os.path.split(license)           # 이미지 명만 추출하기위해 배열로 나눔 split 함수
+    print(license)
+    print(file)
     filepath = "./ccl/" + str(file[1])     # 이미지명만 추출해서 라이선스 이미지를 저장할 경로 삽입
     resultlins(license, filepath)           # 라이선스 이미지 중복 체크
     return filepath
@@ -83,14 +87,14 @@ def information(inserturl, name, path, id_):
     source = html.read()                            # 소스를 읽는다
     html.close()                                    # 모두 진행한 후 close 해준다
 
+    name = name.replace("'", "\\'")                 #' 이 SQL 문자구분에 문제가 생길수도 있으니 \ 삽입
     soup = BeautifulSoup(source, "html5lib")
     selecttable = soup.find(class_='tb_bbs tbType')
     selectimg = soup.find(class_='copyD')
     table = selecttable.find_all('tr')
     lins = selectimg.find('img').get('src')
     lins = "https://gongu.copyright.or.kr" + lins
-    cklins = cklicense(lins)                                        # 라이센스 이미지명만  추출 및 저장 경로 설정
-    lins = cklicense(cklins)                                        # 라이센스 이미지 중복체크
+    lins = cklicense(lins)                                        # 라이센스 이미지 중복체크 및 저장 경로설정
     into_ = "(_id,filename,path,license,"                        # DB에 저장할 컬럼
     value = "('" + id_ + "','" + name + "','" + path + "','" + lins + "',"      # DB에 저장할 값
     count = 4                               # 값들의 갯수 체크 (_id , filename , path , license는 이미 있으므로 4부터 시작)
@@ -101,6 +105,7 @@ def information(inserturl, name, path, id_):
         thtext = th.get_text().strip()                  # th내에 text는 추출하고 공백 제거
         into_ += cktitle(thtext) + ","                  # 값을 넣을 컬럼위치 체크
         tdtext = td.get_text(" ", strip='True')         # td안에서 텍스트 추출 공백이 있을시 띄어쓰기 하나로 변환
+        tdtext = tdtext.replace("'", "\\'")             # ' 이 SQL 문자구분에 문제가 생길수도 있으니 \ 삽입
         value += "'"+tdtext+"',"                        # DB 인서트를 위해 value값 저장
         count += 1                                       # 컬럼갯수 확인
     into_ += "col_size)"                                # 컬럼갯수를 넣을 컬럼 확보 후 괄호 닫기
@@ -118,10 +123,9 @@ def selectid(url):
 
 def ckimg(imgpath, filename, query):
     result = os.path.exists(filename)                               # 중복된 이미지가 있는지 없는지 확인
-    print(imgpath)                                                  # 저장할 img path 확인
     if not result:
         urllib.request.urlretrieve(imgpath, filename)                       # 이미지 폴더에 저장 (저장할 이미지 추출, 저장할 폴더 경로)
-        print(query)
+        print("query : %s " % query)
         print("이미지 저장 성공 filename : %s" % filename)
         cur.execute(query)              #DB 삽입
         print("DB  입력완료")
@@ -131,8 +135,7 @@ def ckimg(imgpath, filename, query):
     return result
 
 
-def Crawling(index):
-    i = 1
+def Crawling(i, index):
     count = 0
     realcount = 0
     while i <= index:
@@ -163,8 +166,30 @@ def Crawling(index):
             if not createimg:                                                             # DB삽입 성공시 성공한 갯수 count
                 count += 1
             print("-----------------------------------------------------------------------------------------------")
-        i += 1000
+        print("----------------------------------------------------")
+        print("           %d 페이지 모두 크롤링 완료                   " % i)
+        print("----------------------------------------------------")
+        i += 1
     print("크롤링완료 새로운 이미지 : (%d/%d)" % (count, realcount))
     print("크롤링한 페이지 %d page" % (i-1))
-input_ = input("크롤링 페이지갯수>> ")
-Crawling(int(input_))
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+def main():
+    result_cr = True
+    while result_cr:
+        input_Ch = input("(1 = 1 ~ 입력값 ｜ 2 = 입력값 ~ 입력값) \n >>")
+        if int(input_Ch) == 1:
+            result_cr = False
+            input_end = input('크롤링페이지 갯수 >>')
+            Crawling(1, int(input_end))
+        elif int(input_Ch) == 2:
+            result_cr = False
+            input_start = input('크롤링 시작 페이지 >> ')
+            input_end = input('크롤링페이지 갯수 >> ')
+            Crawling(int(input_start), int(input_end))
+        else:
+            print("1 과 2중에 하나만 입력해주세요")
+
+main()
