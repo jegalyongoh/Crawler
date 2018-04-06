0850fec6d6f41b6c81356c2c22fde2df84023cf0
unknown <rkfdyddh@gmail.com>
2018-04-06 17:39:24 +0900

    Crawler (ver 2)

--- Crawler.py
+++ Crawler.py
@@ -1,13 +1,25 @@
 #-*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from PIL import Image
 import pymysql
 import urllib
 import os
 import re
-
 conn = pymysql.connect(host='127.0.0.1', port=3307, user='root', passwd='a1234', db='bucketlist', charset='utf8')
 cur = conn.cursor()
+
+
+def endpage():
+    url = "https://gongu.copyright.or.kr/gongu/wrt/wrtCl/listWrt.do?wrtTy=4&menuNo=200023&depth2At=Y"
+    html = urlopen(url)
+    source = html.read()
+    html.close
+    soup = BeautifulSoup(source, 'html5lib')
+    select = soup.find(class_='i end')
+    endpage_ = select.a.get('href')
+    endpage_ = endpage_[-5:]
+    return endpage_
 
 
 def cktitle(title):
@@ -74,15 +86,12 @@ def resultlins(licensepath, path):
 
 def cklicense(license):
     file = os.path.split(license)           # 이미지 명만 추출하기위해 배열로 나눔 split 함수
-    print(license)
-    print(file)
     filepath = "./ccl/" + str(file[1])     # 이미지명만 추출해서 라이선스 이미지를 저장할 경로 삽입
     resultlins(license, filepath)           # 라이선스 이미지 중복 체크
     return filepath
 
 
-def information(inserturl, name, path, id_):
-    url = inserturl
+def information(url, name, path, id_):
     html = urlopen(url)
     source = html.read()                            # 소스를 읽는다
     html.close()                                    # 모두 진행한 후 close 해준다
@@ -90,11 +99,15 @@ def information(inserturl, name, path, id_):
     name = name.replace("'", "\\'")                 #' 이 SQL 문자구분에 문제가 생길수도 있으니 \ 삽입
     soup = BeautifulSoup(source, "html5lib")
     selecttable = soup.find(class_='tb_bbs tbType')
-    selectimg = soup.find(class_='copyD')
+    selectlins = soup.find(class_='copyD')
+    selectimg = soup.find(class_='imgD')
     table = selecttable.find_all('tr')
-    lins = selectimg.find('img').get('src')
+    lins = selectlins.find('img').get('src')
     lins = "https://gongu.copyright.or.kr" + lins
     lins = cklicense(lins)                                        # 라이센스 이미지 중복체크 및 저장 경로설정
+    image = selectimg.find('img').get('src')
+    image = "https://gongu.copyright.or.kr" + image
+
     into_ = "(_id,filename,path,license,"                        # DB에 저장할 컬럼
     value = "('" + id_ + "','" + name + "','" + path + "','" + lins + "',"      # DB에 저장할 값
     count = 4                               # 값들의 갯수 체크 (_id , filename , path , license는 이미 있으므로 4부터 시작)
@@ -111,7 +124,16 @@ def information(inserturl, name, path, id_):
     into_ += "col_size)"                                # 컬럼갯수를 넣을 컬럼 확보 후 괄호 닫기
     value += str(count) + ")"                       # 마지막으로 갯수삽입 후 괄호 닫기
     query = "INSERT INTO crawler"+into_+" values"+value+""      # 쿼리문 작성
-    return query                                     # 쿼리 전송
+    return {'query': query, 'image': image}                                     # 쿼리 전송
+
+
+def thumbnail(path):
+    size = 136, 100
+    image = Image.open(path)
+    image.thumbnail(size)
+    file = os.path.split(path)
+    thumbnail_path = "./thumbnail/" + file[1]
+    image.save(thumbnail_path)
 
 
 def selectid(url):
@@ -124,12 +146,23 @@ def selectid(url):
 def ckimg(imgpath, filename, query):
     result = os.path.exists(filename)                               # 중복된 이미지가 있는지 없는지 확인
     if not result:
-        urllib.request.urlretrieve(imgpath, filename)                       # 이미지 폴더에 저장 (저장할 이미지 추출, 저장할 폴더 경로)
-        print("query : %s " % query)
-        print("이미지 저장 성공 filename : %s" % filename)
-        cur.execute(query)              #DB 삽입
-        print("DB  입력완료")
-        conn.commit()                   #DB commit
+        try:
+            cur.execute(query)              #DB 삽입
+            print("query : %s " % query)
+            conn.commit()                   #DB commit
+            print("DB  입력완료")
+            urllib.request.urlretrieve(imgpath, filename)  # 이미지 폴더에 저장 (저장할 이미지 추출, 저장할 폴더 경로)
+            thumbnail(filename)
+            print("이미지 저장 완료 filename : %s" % filename)
+        except pymysql.err.InternalError:
+            print('error : 새로운 컬럼 필요! 예외 처리했습니다.')
+            result = True
+        except pymysql.err.IntegrityError:
+            print('error : 이미지가 이미 DB에 저장되어있습니다 ')
+            urllib.request.urlretrieve(imgpath, filename)
+            thumbnail(filename)
+            print("이미지 저장 완료 filename : %s" % filename)
+
     else:
         print("이미 저장된 이미지 입니다.")
     return result
@@ -151,25 +184,23 @@ def Crawling(i, index):
 
         for val in li:
             realcount += 1                          # 전체 카운트 인덱싱
-            img = val.find('img').get('src')        # img태그의 src 추출
             href = val.a.get('href')                # li 태그의 href 추출
-            realimg = "https://gongu.copyright.or.kr" + img  # 공유마당 뒷 url 입력
             realhref = "https://gongu.copyright.or.kr" + href
             imgtitle = val.get_text().strip()       # li태그의 텍스트 모두 추출후 공백 지우기
             filename = selectid(realhref)           # selectid 함수로 가서 고유id 추출
-            print("img : %s" % realimg)
             print("href : %s" % realhref)
             print("page : %d" % i)
-            query = information(realhref, imgtitle, filename['path'], filename['id_'])  # 상세정보 추출
-            createimg = ckimg(realimg, filename['path'], query)                          # 폴더 내 중복 이미지 체크 후 DB insert
-
+            info_ = information(realhref, imgtitle, filename['path'], filename['id_'])  # 상세정보 추출
+            createimg = ckimg(info_['image'], filename['path'], info_['query'])      # 폴더 내 중복 이미지 체크 후 DB insert
             if not createimg:                                                             # DB삽입 성공시 성공한 갯수 count
                 count += 1
             print("-----------------------------------------------------------------------------------------------")
+
         print("----------------------------------------------------")
         print("           %d 페이지 모두 크롤링 완료                   " % i)
         print("----------------------------------------------------")
         i += 1
+
     print("크롤링완료 새로운 이미지 : (%d/%d)" % (count, realcount))
     print("크롤링한 페이지 %d page" % (i-1))
 
@@ -178,12 +209,13 @@ def Crawling(i, index):
 
 def main():
     result_cr = True
+
     while result_cr:
-        input_Ch = input("(1 = 1 ~ 입력값 ｜ 2 = 입력값 ~ 입력값) \n >>")
+        input_Ch = input("(1 = 자동화｜ 2 = 입력값 ~ 입력값) \n >>")
         if int(input_Ch) == 1:
             result_cr = False
-            input_end = input('크롤링페이지 갯수 >>')
-            Crawling(1, int(input_end))
+            end = endpage()
+            Crawling(1, int(end))
         elif int(input_Ch) == 2:
             result_cr = False
             input_start = input('크롤링 시작 페이지 >> ')
@@ -191,5 +223,6 @@ def main():
             Crawling(int(input_start), int(input_end))
         else:
             print("1 과 2중에 하나만 입력해주세요")
-
 main()
+
+
